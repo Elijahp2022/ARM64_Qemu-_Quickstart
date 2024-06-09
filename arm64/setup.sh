f55b047b0c99392c9b12749160848bd35e8435e5
Elijah Polyakov <elijah@preparedmind.net>
2024-06-09 19:35:50 -0400

    added setup script

---
+++ arm64/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install qemu-system-arm
+https://cdimage.ubuntu.com/releases/24.04/release/ubuntu-24.04-preinstalled-server-arm64+raspi.img.xz
+xz -d ubuntu-24.04-preinstalled-server-arm64+raspi.img.xz
+ubuntu-24.04-preinstalled-server-arm64+raspi.img.xz
