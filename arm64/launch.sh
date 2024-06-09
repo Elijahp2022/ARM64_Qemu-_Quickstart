dda7a2579da9f033afb4d092dad267002f43cba2
Elijah Polyakov <elijah@preparedmind.net>
2024-06-09 00:04:08 -0400

    Modified launch script

--- arm64/launch.sh
+++ arm64/launch.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
 qemu-system-aarch64 \
 -M virt \
 -m 4G \
 -smp 4 \
--cpu cortex-a53 \
+-cpu cortex-a76 \
 -kernel vmlinuz \
 -initrd initrd.img \
 -drive file=ubuntu-24.04-preinstalled-server-arm64+raspi.img,format=raw,if=none,id=drive0,cache=writeback \
 -device virtio-blk,drive=drive0,bootindex=0 \
--netdev user,id=net0,hostfwd=tcp::2222-:22 \
--device virtio-net-device,netdev=net0 \
 -append 'root=/dev/vda2 noresume rw' \
 -no-reboot \
 -nographic
