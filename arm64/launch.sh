a69c4d4493d49cd7dceee3f4c2e6bfd403852738
Elijah Polyakov <elijah@preparedmind.net>
2024-06-09 18:13:02 -0400

    Re-added internet and fixed initrd in launch script.

--- arm64/launch.sh
+++ arm64/launch.sh
@@ -5,9 +5,11 @@ qemu-system-aarch64 \
 -smp 4 \
 -cpu cortex-a76 \
 -kernel vmlinuz \
--initrd initrd.img \
+-initrd initrd.img-6.8.0-1005-raspi \
 -drive file=ubuntu-24.04-preinstalled-server-arm64+raspi.img,format=raw,if=none,id=drive0,cache=writeback \
 -device virtio-blk,drive=drive0,bootindex=0 \
+-netdev user,id=net0,hostfwd=tcp::2222-:22 \
+-device virtio-net-device,netdev=net0 \
 -append 'root=/dev/vda2 noresume rw' \
 -no-reboot \
 -nographic
