c83ff04bcb6e1ae6e9cc232f4f0be8022e08b3b3
Elijah Polyakov <elijah@preparedmind.net>
2024-06-09 22:18:16 -0400

    Added image resize to setup.

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 sudo apt install qemu-system-arm
 wget https://cdimage.ubuntu.com/releases/24.04/release/ubuntu-24.04-preinstalled-server-arm64+raspi.img.xz
 xz -d ubuntu-24.04-preinstalled-server-arm64+raspi.img.xz
+qemu-img resize ubuntu-24.04-preinstalled-server-arm64+raspi.img +10G
 bash launch.sh
